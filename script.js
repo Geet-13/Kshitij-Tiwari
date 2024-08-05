9ef843dbbf2a8c7c0326066b434143bdbe76c835
Geet-13 <peace.here1310@gmail.com>
2024-08-05 21:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const text = document.querySelector("#msg");
+const text1 = document.querySelector("#msg1");
+const text2 = document.querySelector("#msg2");
 const choices = document.querySelectorAll(".choice");
 const no = document.querySelector("#no");
 const start = (userChoice) => {
     if(userChoice === "no")
     {
-        text.innerText = "This was not the option. See again!";
+        text1.innerText = "This was not the option.";
+        text2.innerText = "See again!"
         no.innerText = "Yes";
         no.id = "yes";
     }
